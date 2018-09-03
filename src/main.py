e3cf8396019d66b3a754ac8f86cb1d84524523b4
Zhehao Wang <wangzhehao410305@gmail.com>
2018-09-03 18:56:00 -0400

    review: add comments; quotes: correct formatting

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     
     quote_reader = QuotesReader(conf_reader['quote-url'])
     quote = select_quote(quote_reader)
-    quote_str = "Quote of the day: " + "\n" + quote['content'] + "\n" + quote["author"]
+    quote_str = "Quote of the day: " + "\n" + quote['content'] + "  -- " + quote["author"]
 
     items_reader = ItemsReader(GSheetClient('client/token.json', 'client/credentials.json'))    
     daily = items_reader.read_daily()
@@ -52,14 +52,21 @@ if __name__ == "__main__":
     greetings_str = "Hi,\n"
     close_str     = "Thanks,\nBot\n\nP.S. remember to update your progress here:\nhttps://docs.google.com/spreadsheets/d/1hloMXB_eL1f_OWpZR3qDSwc51AJQjLslr_yNR0u7n8c/edit"
 
-    todo_list_str = "\n".join(todo_list)
-    todo_list_str = "Consider reviewing the following today:\n" + todo_list_str
+    if len(todo_list) > 0:
+        todo_list_str = "\n - ".join(todo_list)
+        todo_list_str = "Consider reviewing the following today:\n - " + todo_list_str
+    else:
+        todo_list_str = "Looks like there are no items to review today.\n"
 
     today_str = date.today().strftime("%B %d %Y")
-    msg = 'Subject: {}\n\n{}\n{}\n\n{}\n\n{}'.format('Issues for ' + today_str, greetings_str, todo_list_str, quote_str, close_str)
+    msg = 'Subject: {}\n\n{}\n{}\n\n{}\n\n{}'.format('Issues for ' + today_str,
+                                                     greetings_str,
+                                                     todo_list_str,
+                                                     quote_str.encode('utf-8').decode('unicode_escape'),
+                                                     close_str)
 
     print(msg)
 
     emailer = Emailer(conf_reader['email-user'], conf_reader['email-pwd'])
-    # send_email(emailer, conf_reader['email-sender'], [conf_reader['email-recipient']], msg)
+    send_email(emailer, conf_reader['email-sender'], [conf_reader['email-recipient']], msg)
 
--- src/review.py
+++ src/review.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
+import random
 from datetime import datetime, timedelta
+
+"""
+Main business logic class
+"""
 
 class Review():
     def __init__(self):
         self.MAX_REVIEW_ITEM_CNT = 3
         return
 
+    """
+    Given a configuration dictionary, generate a list of review schedules.
+
+    @param  dict <configuration key, configuration value>
+    @return [int], where each element is the number of hours to next review
+    """
     def get_review_schedule(self, confs):
         review_schedule = []
         for i in range(1, 6):
@@ -15,6 +26,16 @@ class Review():
                 review_schedule.append(int(confs[key]))
         return review_schedule
 
+    """
+    Fill review dates in reviews object. Use this when the given review dict
+    may contain less entries than the number of scheduled reviews. (e.g. manually
+    added items to review_backlog without going through daily).
+
+    @param  dict <configuration key, configuration value>
+    @param  dict <review item, {status: str, time: datetime, reschedule_cnt: int}>,
+            modified in-place
+    @return None
+    """
     def fill_review_dates(self, confs, reviews):
         review_schedule = self.get_review_schedule(confs)
         start_time = datetime.now()
@@ -29,6 +50,16 @@ class Review():
                 reviews[key].append(event)
         return
 
+    """
+    Merge the items in review and items read from daily. Ignore the items in daily
+    that are already in review.
+
+    @param  dict <date, {items: str}>
+    @param  dict <configuration key, configuration value>
+    @param  dict <review item, {status: str, time: datetime, reschedule_cnt: int}>,
+            modified in-place
+    @return None
+    """
     def merge_review_items(self, daily, confs, reviews):
         review_schedule = self.get_review_schedule(confs)
         start_time = datetime.now()
@@ -38,9 +69,7 @@ class Review():
             for item in items:
                 key = item.strip()
                 if key:
-                    if key in reviews:
-                        pass
-                    else:
+                    if not key in reviews:
                         # write scheduled review dates, starting from today
                         reviews[key] = []
                         for hour in review_schedule:
@@ -52,6 +81,14 @@ class Review():
                             reviews[key].append(event)
         return
 
+    """
+    Given a merged view of review items, reschedule items whereas needed and
+    generate a todo list up to the number of configured daily review items.
+
+    @param  dict <configuration key, configuration value>
+    @param  dict <review item, {status: str, time: datetime, reschedule_cnt: int}>
+    @return [str]
+    """
     def reschedule_and_generate_todo(self, confs, reviews):
         review_schedule = self.get_review_schedule(confs)
         start_time = datetime.now()
@@ -77,11 +114,19 @@ class Review():
         if 'max-review-items' in confs:
             max_review_items = int(confs['max-review-items'])
 
+        random.shuffle(candidate_items)
         return candidate_items[:max_review_items]
 
     def serialize_single_event(self, event):
         return event['time'].strftime('%m/%d/%y') + '; ' + str(event['status']) + '; ' + str(event['reschedule_cnt'])
 
+    """
+    Given a merged view of review items, convert it to array of arrays ready for
+    writing back to sheet.
+
+    @param  dict <review item, {status: str, time: datetime, reschedule_cnt: int}>
+    @return [[str]]
+    """
     def to_table(self, reviews):
         content = [['Items', 'Review-1', 'Review-2', 'Review-3', 'Review-4', 'Review-5']]
         for key in reviews:
