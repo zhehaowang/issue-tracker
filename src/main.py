5acbc3da4d03b0be1763b9c6d65520c9e1ca41b7
Zhehao Wang <zwang541@bloomberg.net>
2018-11-25 17:42:53 -0500

    main: add exception reporting

--- src/main.py
+++ src/main.py
@@ -22,52 +22,57 @@ def send_email(emailer, sender, receiver, msg):
 if __name__ == "__main__":
     # read content
     conf_reader = ConfReader('conf.json')
+    msg = ''
+    try:
+        quote_reader = QuotesReader(conf_reader['quote-url'])
+        quote = select_quote(quote_reader)
+        quote_str = "Quote of the day: " + "\n" + quote['content'] + "  -- " + quote["author"]
 
-    quote_reader = QuotesReader(conf_reader['quote-url'])
-    quote = select_quote(quote_reader)
-    quote_str = "Quote of the day: " + "\n" + quote['content'] + "  -- " + quote["author"]
+        items_reader = ItemsReader(GSheetClient('client/token.json', 'client/credentials.json'))    
+        daily = items_reader.read_daily()
+        confs = items_reader.read_conf()
+        laws  = items_reader.read_laws()
 
-    items_reader = ItemsReader(GSheetClient('client/token.json', 'client/credentials.json'))    
-    daily = items_reader.read_daily()
-    confs = items_reader.read_conf()
-    laws  = items_reader.read_laws()
+        review_items = items_reader.read_review_backlog()
 
-    review_items = items_reader.read_review_backlog()
+        review = Review()
 
-    review = Review()
+        # fill unscheduled reviews, merge, get todo list
+        review.fill_review_dates(confs, review_items)
+        review.merge_review_items(daily, confs, review_items)
+        todo_list = review.reschedule_and_generate_todo(confs, review_items)
 
-    # fill unscheduled reviews, merge, get todo list
-    review.fill_review_dates(confs, review_items)
-    review.merge_review_items(daily, confs, review_items)
-    todo_list = review.reschedule_and_generate_todo(confs, review_items)
+        # write review schedules back to sheet
+        items_reader.write_review_backlog(review.to_table(review_items))
 
-    # write review schedules back to sheet
-    items_reader.write_review_backlog(review.to_table(review_items))
+        # send email
+        greetings_str = "Hi,\n"
+        close_str     = "Thanks,\nBot\n\nP.S. remember to update your progress here:\nhttps://docs.google.com/spreadsheets/d/1hloMXB_eL1f_OWpZR3qDSwc51AJQjLslr_yNR0u7n8c/edit"
 
-    # send email
-    greetings_str = "Hi,\n"
-    close_str     = "Thanks,\nBot\n\nP.S. remember to update your progress here:\nhttps://docs.google.com/spreadsheets/d/1hloMXB_eL1f_OWpZR3qDSwc51AJQjLslr_yNR0u7n8c/edit"
+        if len(todo_list) > 0:
+            todo_list_str = "\n - ".join(todo_list)
+            todo_list_str = "Consider reviewing the following today:\n - " + todo_list_str
+        else:
+            todo_list_str = "Looks like there are no items to review today.\n"
 
-    if len(todo_list) > 0:
-        todo_list_str = "\n - ".join(todo_list)
-        todo_list_str = "Consider reviewing the following today:\n - " + todo_list_str
-    else:
-        todo_list_str = "Looks like there are no items to review today.\n"
+        laws_str = 'Laws enacted:\n'
+        for law in laws:
+            if law['start_date'] <= date.today() and law['end_date'] >= date.today():
+                laws_str += '  - ' + law['law'] + ' : ' + law['description']
 
-    laws_str = 'Laws enacted:\n'
-    for law in laws:
-        if law['start_date'] <= date.today() and law['end_date'] >= date.today():
-            laws_str += '  - ' + law['law'] + ' : ' + law['description']
-
-    today_str = date.today().strftime("%B %d %Y")
-    msg = 'Subject: {}\n\n{}\n{}\n\n{}\n\n{}\n\n{}'.format('Issues for ' + today_str,
-                                                     greetings_str,
-                                                     todo_list_str,
-                                                     quote_str.encode('utf-8').decode('unicode_escape'),
-                                                     laws_str,
-                                                     close_str)
-
-    print(msg)
+        today_str = date.today().strftime("%B %d %Y")
+        msg = 'Subject: {}\n\n{}\n{}\n\n{}\n\n{}\n\n{}'.format('Issues for ' + today_str,
+                                                         greetings_str,
+                                                         todo_list_str,
+                                                         quote_str.encode('utf-8').decode('unicode_escape'),
+                                                         laws_str,
+                                                         close_str)
+        if __debug__:
+            print(msg)
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except Exception as e:
+        msg = 'Subject: {}\n\n{}'.format('An exception has occurred in issue-tracker', str(e))
 
     emailer = Emailer(conf_reader['email-user'], conf_reader['email-pwd'])
     send_email(emailer, conf_reader['email-sender'], [conf_reader['email-recipient']], msg)
